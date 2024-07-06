4fb749c1c6e12dd1880c3e4ee9906b4b582279c3
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2024-07-06 13:44:06 +0300

    admin theme integrated into the project

---
+++ DapperProject/Areas/Admin/ViewComponents/_AdminNavbarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DapperProject.Areas.Admin.ViewComponents
+{
+    public class _AdminNavbarComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
